e603ff2b91a09738092905879d76c65b0d407668
stoleS <predragstosic29@gmail.com>
2020-04-15 21:17:02 +0200

    Added try/catch

--- controllers/players.js
+++ controllers/players.js
@@ -12,24 +12,26 @@ const addPlayer = async (req, res, next) => {
   res.status(201).send(save);
 };
 const addToWishlist = async (req, res, next) => {
-  const { id } = req.params;
-  const { game } = req.body;
+  try {
+    const { id } = req.params
+    const { game } = req.body
 
-  const player = await Player.findById(id);
-  const igra = await Game.findById(game).catch((error) => {
-    return error;
-  });
-  console.log(igra);
-  if (player.games.includes(game)) {
-    res.status(200).send({ msg: "Igra vec postoji u wishlist-i!" });
-  } else {
-    igra.players.push(id);
-    const save = await igra.save();
-    player.games.push(game);
-    await player.save();
-    res.status(201).send(save);
+    const player = await Player.findById(id)
+    const igra = await Game.findById(game)
+    if (player.games.includes(game)) {
+      res.status(200).send({ msg: 'Igra vec postoji u wishlist-i!' })
+    } else {
+      igra.players.push(id)
+      const save = await igra.save()
+      player.games.push(game)
+      await player.save()
+      res.status(201).send(save)
+    }
+  } catch (error) {
+    res.status(500).send({ error })
   }
-};
+}
+
 const getPlayerById = async (req, res, next) => {
   const { id } = req.params;
   const player = await Player.findById(id).populate("games");
